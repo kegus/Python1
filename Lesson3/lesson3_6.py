ef55d3f2145db260770bcccdc98aa5110a023c59
kegus <kegus@mail.ru>
2020-04-01 16:06:47 +0600

    ДЗ lesson3

---
+++ Lesson3/lesson3_6.py
@@ -0,0 +1,22 @@
+# 6. Реализовать функцию int_func
+def int_func(input_str):
+    result = ''
+    for i, char in enumerate(input_str):
+        if i == 0:
+            char = char.upper()
+        else:
+            char = char.lower()
+        result += char
+    return result
+
+
+def my_title(input_str):
+    result = ''
+    input_list = input_str.split()
+    for item in input_list:
+        item = int_func(item) + ' '
+        result += item
+    return result[:len(result)-1]
+
+print(int_func(input('Введите слово: ')))
+print(my_title(input('Введите предложение: ')))
